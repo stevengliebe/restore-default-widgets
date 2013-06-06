d0317e0a96e6c9971c2f2538da3fe28fba392c0a
Steven Gliebe <work@stevengliebe.com>
2013-06-06 11:57:16 -0500

    Add function to restore widgets

--- restore-default-widgets.php
+++ restore-default-widgets.php
@@ -11,3 +11,44 @@
 
 // No direct access
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Register default widgets
+ *
+ * This repeats the registering that wp_widgets_init() does.
+ * 
+ * @since 1.0
+ */
+function rdw_register_widgets() {
+
+	register_widget( 'WP_Widget_Pages' );
+
+	register_widget( 'WP_Widget_Calendar' );
+
+	register_widget( 'WP_Widget_Archives' );
+
+	if ( get_option( 'link_manager_enabled' ) ) {
+		register_widget( 'WP_Widget_Links' );
+	}
+
+	register_widget( 'WP_Widget_Meta' );
+
+	register_widget( 'WP_Widget_Search' );
+
+	register_widget( 'WP_Widget_Text' );
+
+	register_widget( 'WP_Widget_Categories' );
+
+	register_widget( 'WP_Widget_Recent_Posts' );
+
+	register_widget( 'WP_Widget_Recent_Comments' );
+
+	register_widget( 'WP_Widget_RSS' );
+
+	register_widget( 'WP_Widget_Tag_Cloud' );
+
+	register_widget( 'WP_Nav_Menu_Widget' );
+
+}
+
+add_action( 'widgets_init', 'rdw_register_widgets', 99 ); // likely after unregistering (note: http://10up.com/blog/watch-those-hook-priorities/)
