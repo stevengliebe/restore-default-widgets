833d06c8c3e80129afd1e166e199d10772439f8b
Steven Gliebe <work@stevengliebe.com>
2013-06-10 14:14:53 -0500

    Improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,23 @@
 #! /bin/bash
-# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
+# A modified version of a modified version of deploy.sh.
+# This updates WordPress.org SVN repo from Git repo.
 # 
-# Usage: http://speakinginbytes.com/2012/10/wordpress-plugin-deployment-script/
-#
-# @stevengliebe modifications:
-# - Rename assets-wp-repo to _wporg-assets
-# - Remove automated Git tag/push
-# - Modify some wording
+# Credit:
+# http://speakinginbytes.com/2012/10/wordpress-plugin-deployment-script/
+# https://github.com/deanc/wordpress-plugin-git-svn
 
 # main config
 PLUGINSLUG="restore-default-widgets"
 CURRENTDIR=`pwd`
 MAINFILE="restore-default-widgets.php" # this should be the name of your main php file in the wordpress plugin
 
-# git config
-GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
-
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/restore-default-widgets/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/restore-default-widgets" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="stevengliebe" # your svn username
 
-
 # Let's begin...
+echo 
 echo ".........................................."
 echo 
 echo "Preparing to deploy wordpress plugin"
@@ -31,69 +25,49 @@ echo
 echo ".........................................."
 echo 
 
-# Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
-echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
-echo "$MAINFILE version: $NEWVERSION2"
-
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
-
-echo "Versions match in readme.txt and PHP file. Let's proceed..."
-
-# @stevengliebe: Prefer to Git tag and push manually:
-
-cd $GITPATH
-#echo -e "Enter a commit message for this new version: \c"
-#read COMMITMSG
-#git commit -am "$COMMITMSG"
-
-#echo "Tagging new version in git"
-#git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
-
-#echo "Pushing latest commit to origin, with tags"
-#git push origin master
-#git push origin master --tags
+# Get version
+VERSION=`grep "^Stable tag" "$CURRENTDIR/readme.txt" | awk -F' ' '{print $3}'`
+echo "readme.txt version: $VERSION"
 
 echo 
-echo "Creating local copy of SVN repo ..."
+echo "Creating local copy of SVN repo..."
+rm -fr $SVNPATH/ # remove old tmp
 svn co $SVNURL $SVNPATH
 
-echo "Exporting the HEAD of master from git to the trunk of SVN"
+echo "Exporting the HEAD of master from git to the trunk of SVN..."
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-echo "Ignoring github specific & deployment script"
+echo "Ignoring GitHub-specific and deployment script..."
 svn propset svn:ignore "deploy.sh
 README.md
 .git
 .gitignore" "$SVNPATH/trunk/"
 
-echo "Moving _wporg-assets"
+echo "Moving _wporg-assets..."
 mkdir $SVNPATH/assets/
 mv $SVNPATH/trunk/_wporg-assets/* $SVNPATH/assets/
 svn add $SVNPATH/assets/
-svn delete $SVNPATH/trunk/_wporg-assets
+svn delete --force $SVNPATH/trunk/_wporg-assets
 
-echo "Changing directory to SVN"
+echo "Changing directory to SVN..."
 cd $SVNPATH/trunk/
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-echo "committing to trunk"
-#svn commit --username=$SVNUSER -m "$COMMITMSG"
-svn commit --username=$SVNUSER -m "Uploading version $NEWVERSION1"
+echo "Committing to trunk..."
+svn commit --username=$SVNUSER -m "Uploading version $VERSION"
 
-echo "Updating WP plugin repo assets & committing"
+echo "Creating new SVN tag and committing it..."
+cd $SVNPATH
+svn copy trunk/ tags/$VERSION/
+cd $SVNPATH/tags/$VERSION
+svn commit --username=$SVNUSER -m "Tagging version $VERSION"
+
+echo "Updating WordPress.org plugin repo assets and committing..."
 cd $SVNPATH/assets/
-#svn commit --username=$SVNUSER -m "Updating wp-repo-assets"
+svn add *
 svn commit --username=$SVNUSER -m "Updating repo assets"
 
-echo "Creating new SVN tag & committing it"
-cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
-
-echo "Removing temporary directory $SVNPATH"
+echo "Removing temporary directory $SVNPATH..."
 rm -fr $SVNPATH/
 
 echo "*** FINISHED ***"
